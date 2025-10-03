6f7a112f57a37dad44efbf0193d195bb9de8803c
Igor Fedorov <bullet-1@yandex.ru>
2025-10-03 14:21:29 +0500

    RA-04 add first fragment with categories

---
+++ app/src/main/java/com/ifedorov/recipesapp/CategoriesListFragment.kt
@@ -0,0 +1,28 @@
+package com.ifedorov.recipesapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ifedorov.recipesapp.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
--- app/src/main/java/com/ifedorov/recipesapp/MainActivity.kt
+++ app/src/main/java/com/ifedorov/recipesapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
 import com.ifedorov.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
 
-        //enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (savedInstanceState == null) {
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.mainContainer, CategoriesListFragment())
+            transaction.commit()
         }
     }
 }
