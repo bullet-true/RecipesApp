8f865921ec8795b286292942f0e6bc222b098741
Igor Fedorov <bullet-1@yandex.ru>
2025-10-10 11:37:31 +0500

    RA-10 pass data to recipes screen via Bundle

--- app/src/main/java/com/ifedorov/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/com/ifedorov/recipesapp/CategoriesListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -12,7 +13,8 @@ import com.ifedorov.recipesapp.databinding.FragmentListCategoriesBinding
 class CategoriesListFragment : Fragment() {
     private var _binding: FragmentListCategoriesBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException("FragmentListCategoriesBinding can't be null")
+        get() = _binding
+            ?: throw IllegalStateException("FragmentListCategoriesBinding can't be null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,18 +40,27 @@ class CategoriesListFragment : Fragment() {
         val categoriesListAdapter = CategoriesListAdapter(STUB.getCategories())
 
         categoriesListAdapter.setOnItemClickListener(object : CategoriesListAdapter.OnItemClickListener {
-            override fun onItemClick() {
-                openRecipesByCategoryId()
+            override fun onItemClick(categoryId: Int) {
+                openRecipesByCategoryId(categoryId)
             }
         })
 
         binding.rvCategories.adapter = categoriesListAdapter
     }
 
-    private fun openRecipesByCategoryId() {
+    private fun openRecipesByCategoryId(categoryId: Int) {
+        val categoryName = STUB.getCategories().find { it.id == categoryId }?.title
+        val categoryImageUrl = STUB.getCategories().find { it.id == categoryId }?.imageUrl
+
+        val bundle = bundleOf(
+            "ARG_CATEGORY_ID" to categoryId,
+            "ARG_CATEGORY_NAME" to categoryName,
+            "ARG_CATEGORY_IMAGE_URL" to categoryImageUrl
+        )
+
         parentFragmentManager.commit {
             setReorderingAllowed(true)
-            replace<RecipesListFragment>(R.id.mainContainer)
+            replace<RecipesListFragment>(R.id.mainContainer, args = bundle)
             addToBackStack(null)
         }
     }
--- app/src/main/java/com/ifedorov/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/com/ifedorov/recipesapp/RecipesListFragment.kt
@@ -1,6 +1,7 @@
 package com.ifedorov.recipesapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,10 @@ class RecipesListFragment : Fragment() {
     private val binding
         get() = _binding ?: throw IllegalStateException("FragmentRecipesListBinding can't be null")
 
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +26,16 @@ class RecipesListFragment : Fragment() {
         _binding = FragmentRecipesListBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        categoryId = requireArguments().getInt("ARG_CATEGORY_ID")
+        categoryName = requireArguments().getString("ARG_CATEGORY_NAME")
+        categoryImageUrl = requireArguments().getString("ARG_CATEGORY_IMAGE_URL")
+
+        Log.d("RecipesListFragment", "$categoryId $categoryName $categoryImageUrl")
     }
 
     override fun onDestroyView() {
