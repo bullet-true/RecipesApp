af5696aab7313bde8f0ec024a7924047634ba1d4
Igor Fedorov <bullet-1@yandex.ru>
2025-10-12 14:26:11 +0500

    RA-11 implement recipes list screen layout

---
+++ app/src/main/java/com/ifedorov/recipesapp/RecipesListAdapter.kt
@@ -0,0 +1,57 @@
+package com.ifedorov.recipesapp
+
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ifedorov.recipesapp.databinding.ItemRecipeBinding
+import com.ifedorov.recipesapp.model.Recipe
+import java.io.InputStream
+
+class RecipesListAdapter(private val dataSet: List<Recipe>) :
+    RecyclerView.Adapter<RecipesListAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(recipeId: Int)
+    }
+
+    var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
+
+    class ViewHolder(val binding: ItemRecipeBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val binding = ItemRecipeBinding.inflate(inflater, viewGroup, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val recipe = dataSet[position]
+
+        with(viewHolder.binding) {
+            ivRecipeCardItem.contentDescription = recipe.title
+            tvRecipeCardItemHeader.text = recipe.title
+        }
+
+        try {
+            val inputStream: InputStream? =
+                viewHolder.itemView.context?.assets?.open(recipe.imageUrl)
+            val image = Drawable.createFromStream(inputStream, null)
+            viewHolder.binding.ivRecipeCardItem.setImageDrawable(image)
+        } catch (e: Exception) {
+            Log.e("RecipesListAdapter", "Error loading image: ${recipe.imageUrl}")
+            e.printStackTrace()
+        }
+
+        viewHolder.binding.cardRecipeItemLayout.setOnClickListener {
+            itemClickListener?.onItemClick(recipe.id)
+        }
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+}
