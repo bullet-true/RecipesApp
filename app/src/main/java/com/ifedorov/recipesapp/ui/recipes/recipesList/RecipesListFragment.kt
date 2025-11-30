67026f3dd248f8e06c8c892793e664564fc44203
Igor Fedorov <bullet-1@yandex.ru>
2025-11-30 17:38:47 +0500

    RA-29 add Safe Args and pass recipeId

--- app/src/main/java/com/ifedorov/recipesapp/ui/recipes/recipesList/RecipesListFragment.kt
+++ app/src/main/java/com/ifedorov/recipesapp/ui/recipes/recipesList/RecipesListFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.ifedorov.recipesapp.R
 import com.ifedorov.recipesapp.common.Constants
 import com.ifedorov.recipesapp.databinding.FragmentRecipesListBinding
 
@@ -63,7 +61,7 @@ class RecipesListFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
-        val bundle = bundleOf(Constants.ARG_RECIPE_ID to recipeId)
-        findNavController().navigate(R.id.recipeFragment, bundle)
+        val action = RecipesListFragmentDirections.actionRecipesListFragmentToRecipeFragment(recipeId)
+        findNavController().navigate(action)
     }
 }
