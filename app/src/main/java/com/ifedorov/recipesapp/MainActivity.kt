346e1fcc9187838957bc3cb609ee7e1075ae1fbd
Igor Fedorov <bullet-1@yandex.ru>
2025-12-03 22:14:39 +0500

    RA-32 implement URLConnection request

--- app/src/main/java/com/ifedorov/recipesapp/MainActivity.kt
+++ app/src/main/java/com/ifedorov/recipesapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.ifedorov.recipesapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.navOptions
+import com.google.gson.Gson
 import com.ifedorov.recipesapp.databinding.ActivityMainBinding
+import com.ifedorov.recipesapp.model.Category
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -58,5 +63,35 @@ class MainActivity : AppCompatActivity() {
                 navController.navigate(R.id.favoritesFragment, null, options)
             }
         }
+
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
+        val thread = Thread {
+            Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            val jsonBody = connection.getInputStream().use { inputStream ->
+                inputStream.bufferedReader().use { reader ->
+                    reader.readText()
+                }
+            }
+
+            Log.i("!!!", "Response code: ${connection.responseCode}")
+            Log.i("!!!", "Response message: ${connection.responseMessage}")
+            Log.i("!!!", "Body: $jsonBody")
+
+            val gson = Gson()
+            val categories = gson.fromJson(jsonBody, Array<Category>::class.java)
+
+            categories.forEach { category ->
+                Log.i("!!!", category.title)
+            }
+
+            connection.disconnect()
+        }
+        thread.start()
     }
 }
