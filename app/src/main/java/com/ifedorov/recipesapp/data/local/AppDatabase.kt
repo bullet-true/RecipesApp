dd58c5ea425b0fd8efbbe4fd69bbd88d6d325dc4
Igor Fedorov <bullet-1@yandex.ru>
2025-12-17 14:52:39 +0500

    RA-40 refactor after code review

--- app/src/main/java/com/ifedorov/recipesapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/ifedorov/recipesapp/data/local/AppDatabase.kt
@@ -2,12 +2,15 @@ package com.ifedorov.recipesapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ifedorov.recipesapp.data.local.converters.RecipeTypeConverter
 import com.ifedorov.recipesapp.data.local.dao.CategoriesDao
 import com.ifedorov.recipesapp.data.local.dao.RecipesDao
 import com.ifedorov.recipesapp.model.Category
 import com.ifedorov.recipesapp.model.Recipe
 
 @Database(entities = [Category::class, Recipe::class], version = 1)
+@TypeConverters(RecipeTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoriesDao(): CategoriesDao
     abstract fun recipesDao(): RecipesDao
--- app/src/main/java/com/ifedorov/recipesapp/model/Recipe.kt
+++ app/src/main/java/com/ifedorov/recipesapp/model/Recipe.kt
@@ -4,14 +4,11 @@ import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
-import com.ifedorov.recipesapp.data.local.converters.RecipeTypeConverter
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Parcelize
 @Serializable
-@TypeConverters(RecipeTypeConverter::class)
 @Entity(tableName = "recipes")
 data class Recipe(
     @PrimaryKey val id: Int,
